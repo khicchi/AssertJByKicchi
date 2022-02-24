e6c656d491339010723fadc9c0dd7fdd4d4489a4
ececi <huseyinkiceci@gmail.com>
2022-02-24 21:34:18 +0300

    comm

---
+++ src/test/java/AssertJBasics.java
@@ -0,0 +1,14 @@
+
+import static org.assertj.core.api.Assertions.*;
+
+public class AssertJBasics {
+
+    public static void main(String[] args) {
+
+
+        assertThat(5).isEqualTo(6);
+
+       // System.out.println(assertThat(5).isNotZero().info.toString());
+    }
+
+}
